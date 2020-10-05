624f0d3ab07182fcc0de522828620be89811ab9e
SakiCode <saki91ns@gmail.com>
2020-10-05 08:09:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const baseURL = "https://api.openweathermap.org/data/2.5/";
-const apiKey = "7c0b548ac0f55d93a7af135633e3a048";
+const apiKey = "YOURAPIKEY";
 const units = "metric";
 
 // OWM icons
